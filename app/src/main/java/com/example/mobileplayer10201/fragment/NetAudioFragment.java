cb5dccff64eee8d9103608e39106a80bc7e0784e
xm890419 <506083998@qq.com>
2017-01-16 13:48:09 +0800

    解析数据-gson解析和gsonFormat和手动解析

--- app/src/main/java/com/example/mobileplayer10201/fragment/NetAudioFragment.java
+++ app/src/main/java/com/example/mobileplayer10201/fragment/NetAudioFragment.java
@@ -5,14 +5,19 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.mobileplayer10201.NetAudioBean;
 import com.example.mobileplayer10201.R;
 import com.example.mobileplayer10201.base.BaseFragment;
 import com.example.mobileplayer10201.utils.CacheUtils;
 import com.example.mobileplayer10201.utils.Constants;
+import com.google.gson.Gson;
 
 import org.xutils.common.Callback;
+import org.xutils.common.util.LogUtil;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -45,7 +50,6 @@ public class NetAudioFragment extends BaseFragment {
     public void initData() {
         super.initData();
         String saveJson = CacheUtils.getString(mContext, Constants.NET_AUDIO_URL);
-
         getDataFromNet();
     }
 
@@ -56,24 +60,37 @@ public class NetAudioFragment extends BaseFragment {
             public void onSuccess(String result) {
 
                 CacheUtils.putString(mContext, Constants.NET_AUDIO_URL, result);
+                LogUtil.e("onSuccess==" + result);
+
             }
 
             @Override
             public void onError(Throwable ex, boolean isOnCallback) {
-
+                LogUtil.e("onError==" + ex.getMessage());
             }
 
             @Override
             public void onCancelled(CancelledException cex) {
-
+                LogUtil.e("onCancelled==" + cex.getMessage());
             }
 
             @Override
             public void onFinished() {
-
+                LogUtil.e("onFinished==");
             }
         });
     }
+
+    /**
+     * 使用Gson解析json数据
+     * @param json
+     * @return
+     */
+    private List<NetAudioBean.ListBean> parsedJson(String json) {
+        NetAudioBean netAudioBean = new Gson().fromJson(json,NetAudioBean.class);
+        return netAudioBean.getList();
+    }
+
     @Override
     public void onRefreshData() {
         super.onRefreshData();
